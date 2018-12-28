0f96ab6d8ee3bb72592236ead49db561aa62763c
Chris Laughlin <bluntm@gmail.com>
2018-12-28 20:59:44 +0000

    fixing build error

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import {
     FacebookShareButton,
     GooglePlusShareButton,
@@ -12,27 +12,43 @@ import {
     EmailIcon,
 } from 'react-share';
 
-const ShareButtons = () => {
-    const shareUrl = window.location.href;
-    return (
-        <div className="share-buttons">
-            <FacebookShareButton url={shareUrl}>
-                <FacebookIcon round size={32}/>
-            </FacebookShareButton>
-            <GooglePlusShareButton url={shareUrl}>
-                <TwitterIcon round size={32}/>
-            </GooglePlusShareButton>
-            <LinkedinShareButton url={shareUrl}>
-                <GooglePlusIcon round size={32}/>
-            </LinkedinShareButton>
-            <TwitterShareButton url={shareUrl}>
-                <LinkedinIcon round size={32}/>
-            </TwitterShareButton>
-            <EmailShareButton url={shareUrl}>
-                <EmailIcon round size={32}/>
-            </EmailShareButton>
-        </div>
-    )
+class ShareButtons extends PureComponent {
+
+    constructor() {
+        super();
+        this.state = {
+            shareUrl: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setState({shareUrl: window.location.href})
+    }
+
+    render() {
+        const {
+            shareUrl
+        } = this.state;
+        return (
+            <div className="share-buttons">
+                <FacebookShareButton url={shareUrl}>
+                    <FacebookIcon round size={32}/>
+                </FacebookShareButton>
+                <GooglePlusShareButton url={shareUrl}>
+                    <TwitterIcon round size={32}/>
+                </GooglePlusShareButton>
+                <LinkedinShareButton url={shareUrl}>
+                    <GooglePlusIcon round size={32}/>
+                </LinkedinShareButton>
+                <TwitterShareButton url={shareUrl}>
+                    <LinkedinIcon round size={32}/>
+                </TwitterShareButton>
+                <EmailShareButton url={shareUrl}>
+                    <EmailIcon round size={32}/>
+                </EmailShareButton>
+            </div>
+        )
+    }
 }
 
 
